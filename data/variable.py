e1fa502efe5de1b5bbdb3977d5b71b6d9ba73a11
Kareem Morsy <kreem.morsy@hotmail.com>
2022-12-15 20:10:15 +0200

    Variable update

--- data/variable.py
+++ data/variable.py
@@ -1,12 +1,18 @@
 from .fuzzy_set import FuzzySet
+from typing import Literal
+
+IN = "IN"
+OUT = "OUT"
 
 
 class Variable:
     fuzzySets: list[FuzzySet]
 
-    def __init__(self, name="") -> None:
+    def __init__(self, type: Literal["IN", "OUT"], limits: tuple[int, int], name="") -> None:
         self.name = name
         self.fuzzySets = []
+        self.type = type
+        self.limits = limits
     
     def addFuzzySet(self, fuzzySet: FuzzySet):
         self.fuzzySets.append(fuzzySet)
